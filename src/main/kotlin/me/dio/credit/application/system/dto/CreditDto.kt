59ff8011344a6ea038099ab422e650ad084bbd65
Wagner Nogueira <wbnogueira3@gmail.com>
2023-10-03 15:31:57 -0300

    Incluso as Exception e o README

--- src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
@@ -1,15 +1,17 @@
 package me.dio.credit.application.system.dto
 
+import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.NotNull
 import me.dio.credit.application.system.entity.Credit
 import me.dio.credit.application.system.entity.Customer
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class CreditDto(
-    val creditValue: BigDecimal,
-    val dayFirstOfInstallment: LocalDate,
+    @field:NotNull(message = "The value of the credit cannot be null") val creditValue: BigDecimal,
+    @field:Future(message = "The first of installment cannot be in the past")val dayFirstOfInstallment: LocalDate,
     val numberOfInstallments: Int,
-    val customerId: Long
+    @field:NotNull(message = "The customer cannot be null") val customerId: Long
 ) {
 
     fun toEntity(): Credit = Credit(
--- src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
+++ src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
@@ -1,6 +1,7 @@
 package me.dio.credit.application.system.service.impl
 
 import me.dio.credit.application.system.entity.Credit
+import me.dio.credit.application.system.exception.BusinessException
 import me.dio.credit.application.system.repository.CreditRepository
 import me.dio.credit.application.system.service.ICreditService
 import org.springframework.stereotype.Service
@@ -23,7 +24,7 @@ class CreditService(
 
     override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
         val credit: Credit = (this.creditRepository.findByCreditCode(creditCode)
-            ?: throw RuntimeException("Creditcode $creditCode not found"))
-        return if (credit.customer?.id == customerId) credit else throw RuntimeException("Contact admin")
+            ?: throw BusinessException("Creditcode $creditCode not found"))
+        return if (credit.customer?.id == customerId) credit else throw IllegalArgumentException("Contact admin")
     }
 }
